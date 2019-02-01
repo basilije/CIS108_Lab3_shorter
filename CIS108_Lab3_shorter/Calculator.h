918fb1a0e0b77d9f9bf2b0257904fadce3dc3b7c
basilije <basilije@users.noreply.github.com>
2019-02-01 14:16:39 -0800

    Update Calculator.h

--- CIS108_Lab3_shorter/Calculator.h
+++ CIS108_Lab3_shorter/Calculator.h
@@ -3,4 +3,12 @@
 
 using namespace std;
 
-string makeOperation(string first_number, string operation, string second_number);
+double addition(double first_number, double second_number);
+double subtraction(double first_number, double second_number);
+double multiplication(double first_number, double second_number);
+double division(double first_number, double second_number);
+double powerFunction(double first_number, double second_number);
+void memoryStore(double input_number);
+double memoryRecall();
+void memoryClear();
+double invertSign(double input_number);
