7c291709148bd6cd82ff6d16be0bb8e2c0836af1
basilije <basilije@users.noreply.github.com>
2019-02-13 18:15:02 -0800

    Update Calculator.h
    adding the namespace

--- Exercise1/Calculator.h
+++ Exercise1/Calculator.h
@@ -1,29 +1,30 @@
 #pragma once
 
+namespace Calculator{
+
+	double makeAddition(double first_number, double second_number);
 
 
-double makeAddition(double first_number, double second_number);
+	double makeSubtraction(double first_number, double second_number);
 
 
-double makeSubtraction(double first_number, double second_number);
+	double makeMultiplication(double first_number, double second_number);
 
 
-double makeMultiplication(double first_number, double second_number);
+	double makeDivision(double first_number, double second_number);
 
 
-double makeDivision(double first_number, double second_number);
+	double makePower(double first_number, double second_number);
 
 
-double makePower(double first_number, double second_number);
+	void memoryStore(double input_number);
 
 
-void memoryStore(double input_number);
+	double memoryRecall();
 
 
-double memoryRecall();
+	void memoryClear();
 
 
-void memoryClear();
-
-
-double invertSign(double input_number);
+	double invertSign(double input_number);
+}
